427ba61c368cb758bb866ee25f67cbec7d205a7f
milicab96 <milicabastric@gmail.com>
2024-04-05 20:07:04 +0200

    initial commit

---
+++ training-service/src/main.ts
@@ -0,0 +1,18 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+import { HttpStatus } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+export async function healthcheck() {
+  // const app = await NestFactory.create(AppModule);
+  return {
+    statusCode: HttpStatus.OK,
+    body: JSON.stringify('All good'),
+  };
+}
